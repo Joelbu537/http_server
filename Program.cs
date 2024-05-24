0147ac087a2b59148fae1d4e316e800b41200f8c
2.Account <151151449+FinnAller@users.noreply.github.com>
2024-05-24 10:33:10 +0200

    Update Program.cs
    Added HTML5 support for 404 responses

--- Program.cs
+++ Program.cs
@@ -119,8 +119,8 @@ namespace HttpListenerExample
                         // Handle unknown endpoints (send 404)
                         resp.StatusCode = 404;
                         resp.StatusDescription = "Not Found";
-                        byte[] notFoundData = Encoding.UTF8.GetBytes("404 - Page not found");
-                        resp.ContentType = "text/plain";
+                        byte[] notFoundData = Encoding.UTF8.GetBytes(Path.Combine(Directory.GetCurrentDirectory(), "404.html");
+                        resp.ContentType = "text/html";
                         resp.ContentEncoding = Encoding.UTF8;
                         resp.ContentLength64 = notFoundData.LongLength;
                         await resp.OutputStream.WriteAsync(notFoundData, 0, notFoundData.Length);
